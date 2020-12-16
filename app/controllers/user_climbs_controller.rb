98a00cf85bd19ddf03db1a496a7ee6c229836e59
jrwatson94 <j.watsonreid@gmail.com>
2020-12-15 19:08:42 -0500

    updating controllers

--- app/controllers/user_climbs_controller.rb
+++ app/controllers/user_climbs_controller.rb
@@ -1,5 +1,10 @@
 class UserClimbsController < ApplicationController
-    skip_before_action :authorized, only: [:index, :show]
+    skip_before_action :authorized
+    def index 
+        user_climbs = UserClimb.all
+        render json: user_climbs
+    end
+    
     def new 
         user_climb = UserClimb.new
         render json: user_climb
@@ -9,8 +14,15 @@ class UserClimbsController < ApplicationController
         user_climb = UserClimb.create(user_climb_params)
         render json: user_climb
     end
+    def destroy
+        user_climb = UserClimb.find(params[:id])
+        user_climb.destroy
+    end
+
+    private
 
     def user_climb_params
         params.require(:user_climb).permit(:user_id, :climb_id, :completed, :favorite)
     end
+
 end
--- app/models/user_climb.rb
+++ app/models/user_climb.rb
@@ -1,4 +1,6 @@
 class UserClimb < ApplicationRecord
     belongs_to :user
     belongs_to :climb
+
+    validates_uniqueness_of :user_id, :scope => :climb_id
 end
